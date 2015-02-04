4676d087663c295c37b1f960fb76f6310a819857
Ciel <imwithye@gmail.com>
2015-02-04 16:57:00 +0800

    Add unit test.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,59 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        clean: ['brainish/'],
-        copy: {
-            main: {
-                expand: true,
-                cwd: 'src',
-                src: ['**/*.js'],
-                dest: 'lib/',
-                filter: 'isFile'
-            }
-        },
-        coffee: {
-            compile: {
-                expand: true,
-                cwd: 'src',
-                src: ['**/*.coffee'],
-                dest: 'brainish/',
-                ext: '.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>) */\n'
-            },
-            all: {
-                files: [{
-                    expand: true,
-                    cwd: 'brainish',
-                    src: ['**/*.js'],
-                    dest: 'brainish',
-                    ext: '.js'
-                }]
-            }
-        }
-    });
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ['brainish/', 'brainish_test/'],
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src',
+        src: ['**/*.js'],
+        dest: 'lib/',
+        filter: 'isFile'
+      }
+    },
+    coffee: {
+      compile: {
+        expand: true,
+        cwd: 'src',
+        src: ['**/*.coffee'],
+        dest: 'brainish/',
+        ext: '.js'
+      },
+      compile_test: {
+        expand: true,
+        cwd: 'test',
+        src: ['**/*.coffee'],
+        dest: 'brainish_test/',
+        ext: '.js'
+      }
+    },
+    nodeunit: {
+      all: ['brainish_test/**/*_test.js']
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>) */\n'
+      },
+      all: {
+        files: [{
+          expand: true,
+          cwd: 'brainish',
+          src: ['**/*.js'],
+          dest: 'brainish',
+          ext: '.js'
+        }]
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-coffee');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    // Default task(s).
-    grunt.registerTask('default', ['clean', 'copy', 'coffee', 'uglify']);
+  // Default task(s).
+  grunt.registerTask('default', ['clean', 'copy', 'coffee', 'uglify', 'nodeunit']);
 };
